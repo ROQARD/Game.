360c659ccafda2704b6265bacd0716d62d406ae8
ROQARD <rooqard@gmail.com>
2026-03-05 19:39:13 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 export default {
   players: {},
   obstacles: [
-    {x: 0, y: 600, w: 2000, h: 50}, // Floor
-    {x: 400, y: 450, w: 200, h: 30},
-    {x: 800, y: 350, w: 200, h: 30}
+    {x: 0, y: 500, w: 1000, h: 50}, // Starting floor
+    {x: 1200, y: 400, w: 100, h: 20},
+    {x: 1500, y: 300, w: 100, h: 20}
   ],
   async fetch(request, env) {
     if (request.headers.get('Upgrade') === 'websocket') {
       const [client, server] = Object.values(new WebSocketPair());
       server.accept();
       
-      // Infinite Obstacle Loop
-      if (!this.started) {
-        this.started = true;
+      if (!this.init) {
+        this.init = true;
         setInterval(() => {
           this.obstacles.forEach(o => {
-            o.x -= 5; // The "Scroll" speed
-            if (o.x + o.w < 0) {
-              o.x = 1000 + Math.random() * 500;
+            o.x -= 6; // Fast scrolling
+            if (o.x + o.w < -100) {
+              o.x = 1200 + Math.random() * 400;
               o.y = 200 + Math.random() * 400;
+              o.w = 50 + Math.random() * 100; // Random widths
             }
           });
         }, 16);
@@ -31,6 +31,6 @@ export default {
       });
       return new Response(null, { status: 101, webSocket: client });
     }
-    return new Response("Server Active");
+    return new Response("Game Server Running");
   }
 };
