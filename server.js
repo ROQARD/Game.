5579195f2671a73fb7c5afc0ea8553166a4a9b92
ROQARD <rooqard@gmail.com>
2026-03-05 19:48:07 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
 export default {
   players: {},
-  obstacles: [
-    {x: 0, y: 500, w: 1000, h: 50}, // Starting floor
-    {x: 1200, y: 400, w: 100, h: 20},
-    {x: 1500, y: 300, w: 100, h: 20}
-  ],
+  obstacles: [],
+  
   async fetch(request, env) {
     if (request.headers.get('Upgrade') === 'websocket') {
       const [client, server] = Object.values(new WebSocketPair());
       server.accept();
-      
-      if (!this.init) {
-        this.init = true;
-        setInterval(() => {
-          this.obstacles.forEach(o => {
-            o.x -= 6; // Fast scrolling
-            if (o.x + o.w < -100) {
-              o.x = 1200 + Math.random() * 400;
-              o.y = 200 + Math.random() * 400;
-              o.w = 50 + Math.random() * 100; // Random widths
-            }
-          });
-        }, 16);
+
+      // Generate a vertical tower if it doesn't exist
+      if (this.obstacles.length === 0) {
+          this.obstacles.push({x: 0, y: 500, w: 400, h: 50}); // Base
+          for(let i=1; i<100; i++) {
+              this.obstacles.push({
+                  x: Math.random() * 300,
+                  y: 500 - (i * 150),
+                  w: 80, h: 15
+              });
+          }
       }
 
       server.addEventListener('message', (msg) => {
@@ -29,8 +24,9 @@ export default {
         this.players[d.id] = { x: d.x, y: d.y, name: d.name };
         server.send(JSON.stringify({ players: this.players, obstacles: this.obstacles }));
       });
+
       return new Response(null, { status: 101, webSocket: client });
     }
-    return new Response("Game Server Running");
+    return new Response("Climber Server Active");
   }
 };
